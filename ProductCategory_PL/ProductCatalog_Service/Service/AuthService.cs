94ec55737e95fe16e613d06ceeb5e57531c9f2bf
Abdelrahman-Muhmed <abdogomaa18@yahoo.com>
2025-02-14 01:36:09 +0200

    Handle Service Layer

---
+++ ProductCategory_PL/ProductCatalog_Service/Service/AuthService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ProductCatalog_BLL.IService;
+using ProductCatalog_DAL.Models.IdentityModel;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProductCatalog_Service.ServiceRepo
+{
+    public class AuthService : IAuthServic
+    {
+        private readonly IConfiguration _configuration;
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<string> CreateTokenAsync(ApplicationUser user, UserManager<ApplicationUser> userManager)
+        {
+            // Start Create Token By Claim 
+            //1- Create New Claim 
+
+            var authCliam = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name , user.firstName + " " + user.lastName),
+                new Claim(ClaimTypes.Email , user.Email),
+				new Claim(ClaimTypes.NameIdentifier,user.Id.ToString())
+
+			};
+
+            // 2- Create Roles For Users 
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            // 3- Get Roles With By Forech For Add Roles For Claim 
+            foreach (var Role in userRoles)
+            {
+                authCliam.Add(new Claim(ClaimTypes.Role, Role));
+            }
+
+
+            // 4- Create SymmtricKey 
+            //var SymmetricKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("dsfsfda6666adf6daffafadddddd6fad45afd"));
+            //Or We Can create it in appsetting 
+            var authKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:AuthKey"] ?? string.Empty));
+
+
+            // 5- Create RegisterClaim 
+
+            var token = new JwtSecurityToken(
+
+                audience: _configuration["JWT:audience"],
+                issuer: _configuration["JWT:issure"],
+                expires: DateTime.UtcNow.AddDays(3),
+                claims: authCliam,
+                signingCredentials: new SigningCredentials(authKey, SecurityAlgorithms.HmacSha256Signature)
+                );
+
+            // 6- Return 
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ ProductCategory_PL/ProductCatalog_Service/Service/LoggingService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using ProductCatalog_BLL.IService;
+
+namespace ProductCatalog_Service.ServiceRepo
+{
+
+    public class LoggingService : ILoggingService
+    {
+        private readonly ILogger<LoggingService> _logger;
+        public LoggingService(ILogger<LoggingService> logger)
+        {
+            _logger = logger;
+        }
+        public void LogInformation(string message)
+        {
+            _logger.LogInformation(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            _logger.LogWarning(message);
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            _logger.LogError(ex, message);
+        }
+    }
+}
