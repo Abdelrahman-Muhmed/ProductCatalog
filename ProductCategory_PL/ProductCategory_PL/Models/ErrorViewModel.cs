7e6f1e49df4cb89b0b5951ab8dddab759a053c84
Abdelrahman-Muhmed <abdogomaa18@yahoo.com>
2025-02-14 02:13:59 +0200

    Handle Log File , Error Handling And Meddle Ware

--- ProductCategory_PL/ProductCategory_PL/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProductCategory_PL.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
