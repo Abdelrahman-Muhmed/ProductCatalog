ce4bb1f69260a6963ee7076e4cd945c913a82b67
Abdelrahman-Muhmed <abdogomaa18@yahoo.com>
2025-02-16 15:48:35 +0200

    update

--- ProductCategory_PL/ProductCatalog_DAL/Prsistence/Repository/GenericRepository.cs
+++ ProductCategory_PL/ProductCatalog_DAL/Prsistence/Repository/GenericRepository.cs
@@ -8,55 +8,49 @@ namespace ProductCatalog_DAL.Repository
     public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
         private readonly ProductContext _dbContext;
-        public GenericRepository(ProductContext dbContext)
+	
+
+		public GenericRepository(ProductContext dbContext)
         {
             _dbContext = dbContext;
-        }
+			
 
-        public async Task<IReadOnlyList<T>> GetAllAsync()
-        {
-            return await _dbContext.Set<T>().AsNoTracking().ToListAsync();
-        }
+		}
 
-        public async Task<T?> GetAsync(int? id)
-        {
-            return await _dbContext.Set<T>().FindAsync(id);
-        }
+		public async Task<IReadOnlyList<T>> GetAllAsync()
+		{
+			return await _dbContext.Set<T>().AsNoTracking().ToListAsync();
+		}
 
-        public async Task<T> AddAsync(T entity)
-        {
-            await _dbContext.AddAsync(entity);
-            await _dbContext.SaveChangesAsync();
-            return entity;
-        }
-        public async Task<T> DeleteAsync(int id)
-        {
-            var entity = await GetAsync(id);
-            if (entity != null)
-            {
-                _dbContext.Set<T>().Remove(entity);
-                await _dbContext.SaveChangesAsync();
-                return entity;
-            }
-            return null;
-        }
-        public async Task<T> UpdateAsync(T entity)
-        {
-            _dbContext.Set<T>().Update(entity);
-            await _dbContext.SaveChangesAsync();
-            return entity;
-        }
+		public async Task<T?> GetAsync(int? id)
+		{
+			return await _dbContext.Set<T>().FindAsync(id);
+		}
 
-        //public void Add(T entity)
-        // => _dbContext.Set<T>().Add(entity);
+		public async Task<T> AddAsync(T entity)
+		{
+			await _dbContext.AddAsync(entity);
+			return entity;
+		}
 
-        //public void Update(T entity)
-        //=> _dbContext.Set<T>().Update(entity);
+		public async Task<T> DeleteAsync(int id)
+		{
+			var entity = await GetAsync(id);
+			if (entity != null)
+			{
+				_dbContext.Set<T>().Remove(entity);
+			}
+			return entity;
+		}
 
-        //public void Delete(T entity)
-        //=> _dbContext.Set<T>().Remove(entity);
+		public async Task<T> UpdateAsync(T entity)
+		{
+			_dbContext.Set<T>().Update(entity);
+			return entity;
+		}
 
 
 
-    }
+
+	}
 }
